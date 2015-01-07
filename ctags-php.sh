361962ab83b0f82f8bc3355b8c44056a967de580
Kozie <mail@kozie.nl>
2015-01-07 16:02:13 +0100

    Update ctags-php.sh

--- ctags-php.sh
+++ ctags-php.sh
@@ -1,11 +1,11 @@
-  1 #!/bin/bash
-  2 ctags -f ./tags \
-  3   -h \".php\" -R \
-  4   --exclude=\"\.svn\" \
-  5   --exclude=\"*\.js\" \
-  6   --totals=yes \
-  7   --tag-relative=yes \
-  8   --PHP-kinds=+cf \
-  9   --regex-PHP='/abstract class ([^ ]*)/\1/c/' \
- 10   --regex-PHP='/interface ([^ ]*)/\1/c/' \
- 11   --regex-PHP='/(public |static |abstract |protected |private )+function ([^ (]*)/\2/f/'
+#!/bin/bash
+ctags -f ./tags \
+   -h \".php\" -R \
+   --exclude=\"\.svn\" \
+   --exclude=\"*\.js\" \
+   --totals=yes \
+   --tag-relative=yes \
+   --PHP-kinds=+cf \
+   --regex-PHP='/abstract class ([^ ]*)/\1/c/' \
+   --regex-PHP='/interface ([^ ]*)/\1/c/' \
+   --regex-PHP='/(public |static |abstract |protected |private )+function ([^ (]*)/\2/f/'
