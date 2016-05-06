c499c0c73a288877d9b4aaa807c9267b4d00c8b1
Kozie <mail@kozie.nl>
2016-05-06 17:01:48 +0200

    Update setupvim.sh

--- setupvim.sh
+++ setupvim.sh
@@ -1,1 +1,8 @@
-echo "Setting up vim"
+echo "Installing vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Getting vimrc"
+wget -O ~/.vimrc https://kozie.github.io/dotfiles/vimrc-simple
+
+echo "Installing vim plugins"
+vim +PluginInstall +qall
