1f5c89e734ef4b0357545fa2a6433b698a385f7f
Kozie <mail@kozie.nl>
2016-07-19 15:41:14 +0200

    Create paarde.sh

---
+++ paarde.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -n 'Hoi paarde, wat is je naam? Mijn naam is '
+read naam
+
+say -v 'Ellen' "Hallo $naam. Jij bent echt een godsvergeten, paarde. Zo paarde,
+dat je paarde paarde bent met paarde paarde, en paarde, klote paarde. Paarde
+$naam. paarde paarde $naam, $naam, paarde. En alles er om heen is paarde.
+$naam?"
