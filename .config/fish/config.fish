b94e97ea4d2c6d5f0f1ba0dceb932656faed22c5
Koos van Egmond <koos@cream.nl>
2017-05-10 10:16:08 +0200

    Forgot about the $argv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -86,49 +86,49 @@ function g     ; git $argv ; end
 
 # aliases
 # TODO change to functions to win a bunch of ms startup time!
-function v      ; eval $EDITOR; end
-function vi     ; eval $EDITOR; end
+function v      ; eval $EDITOR $argv; end
+function vi     ; eval $EDITOR $argv; end
 
-function vgs    ; vagrant global-status; end
-function up     ; vagrant up; end
-function down   ; vagrant halt -f; end
-function vssh   ; vagrant ssh; end
+function vgs    ; vagrant global-status $argv; end
+function up     ; vagrant up $argv; end
+function down   ; vagrant halt -f $argv; end
+function vssh   ; vagrant ssh $argv; end
 
-function co     ; git checkout; end
-function cm     ; git commit; end
-function add    ; git add; end
-function push   ; git push; end
-function pull   ; git pull; end
-function fetch  ; git fetch; end
-function merge  ; git merge --no-ff; end
-function qmerge ; git merge --no-edit --no-ff; end
-function qm     ; git merge --no-edit --no-ff; end # Same as previous
-function st     ; git status; end # Same as previous
-function lol    ; git log --graph --decorate --pretty=oneline --abbrev-commit; end
-function lola   ; git log --graph --decorate --pretty=oneline --abbrev-commit --all; end
+function co     ; git checkout $argv; end
+function cm     ; git commit $argv; end
+function add    ; git add $argv; end
+function push   ; git push $argv; end
+function pull   ; git pull $argv; end
+function fetch  ; git fetch $argv; end
+function merge  ; git merge --no-ff $argv; end
+function qmerge ; git merge --no-edit --no-ff $argv; end
+function qm     ; git merge --no-edit --no-ff $argv; end # Same as previous
+function st     ; git status $argv; end # Same as previous
+function lol    ; git log --graph --decorate --pretty=oneline --abbrev-commit $argv; end
+function lola   ; git log --graph --decorate --pretty=oneline --abbrev-commit --all $argv; end
 
-function tm     ; tmux new -s; end
-function ta     ; tmux a; end
-function tas    ; tmux a -t; end
+function tm     ; tmux new -s $argv; end
+function ta     ; tmux a $argv; end
+function tas    ; tmux a -t $argv; end
 
 function water  ; source ~/.config/fish/config.fish; and fish_user_key_bindings; end
 function vish   ; eval $EDITOR ~/.config/fish/config.fish; and water; end # source ~/.config/fish/config.fish"
 function vizsh  ; vish; end
-function ls     ; command ls --color=auto; end
-function ll     ; ls --color=auto -laF; end
-function ag     ; ag --path-to-ignore ~/.agignore; end
+function ls     ; command ls --color=auto $argv; end
+function ll     ; ls --color=auto -laF $argv; end
+function ag     ; ag --path-to-ignore ~/.agignore $argv; end
 
 # Search (file) and find (in file) using ag
-function s      ; ag --path-to-ignore ~/.agignore -Quil; end
-function f      ; ag --path-to-ignore ~/.agignore -Qui; end
+function s      ; ag --path-to-ignore ~/.agignore -Quil $argv; end
+function f      ; ag --path-to-ignore ~/.agignore -Qui $argv; end
 
 function storm  ; open -a 'PhpStorm 2017.1 EAP'; end
 
-function mr     ; n98-magerun; end
-function mr2    ; n98-magerun2; end
+function mr     ; n98-magerun $argv; end
+function mr2    ; n98-magerun2 $argv; end
 function cc     ; mr cache:clean; and mr cache:flush; and date; end
 function cc2    ; mr2 cache:flush; and date; end
-function phpm   ; php -d memory_limit=-1; end
+function phpm   ; php -d memory_limit=-1 $argv; end
 
 # load up extra (private) stuff
 if test -e ~/.config/fish/extra.fish; source ~/.config/fish/extra.fish; end;
